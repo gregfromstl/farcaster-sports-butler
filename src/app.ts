e8d8cd429557866d00ab62aedebde96c917df2f3
gregfromstl <gregfromstl@gmail.com>
2024-01-08 18:20:18 -0800

    ✨ feat: Reports messages to send to console

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,68 @@
 import cron from "node-cron";
-import { MESSAGE } from "./utils";
 import neynarClient from "./neynarClient";
-import {
-  PUBLISH_CAST_TIME,
-  SIGNER_UUID,
-  TIME_ZONE,
-  NEYNAR_API_KEY,
-  FARCASTER_BOT_MNEMONIC,
-} from "./config";
+import { SIGNER_UUID, NEYNAR_API_KEY, FARCASTER_BOT_MNEMONIC } from "./config";
 import { isApiErrorResponse } from "@neynar/nodejs-sdk";
+import { getLatestScores } from "./scores";
+
+const existingScores = new Set();
 
 // Validating necessary environment variables or configurations.
 if (!FARCASTER_BOT_MNEMONIC) {
-  throw new Error("FARCASTER_BOT_MNEMONIC is not defined");
+    throw new Error("FARCASTER_BOT_MNEMONIC is not defined");
 }
 if (!SIGNER_UUID) {
-  throw new Error("SIGNER_UUID is not defined");
+    throw new Error("SIGNER_UUID is not defined");
 }
 
 if (!NEYNAR_API_KEY) {
-  throw new Error("NEYNAR_API_KEY is not defined");
+    throw new Error("NEYNAR_API_KEY is not defined");
 }
+
+/**
+ * Function to publish all new finalized scores.
+ */
+const publishScores = async () => {
+    const scores = await getLatestScores(); // Getting the latest scores.
+    const newScores = scores.filter((score) => !existingScores.has(score.id)); // Filtering out the scores that have already been published.
+
+    newScores.forEach((score) => {
+        const msg = `FINAL SCORE\n${score.scores[0].name}: ${score.scores[0].score}\n${score.scores[1].name}: ${score.scores[1].score}`;
+        publishCast(msg);
+        existingScores.add(score.id);
+    });
+};
 
 /**
  * Function to publish a message (cast) using neynarClient.
  * @param msg - The message to be published.
  */
 const publishCast = async (msg: string) => {
-  try {
-    // Using the neynarClient to publish the cast.
-    await neynarClient.publishCast(SIGNER_UUID, msg);
-    console.log("Cast published successfully");
-  } catch (err) {
-    // Error handling, checking if it's an API response error.
-    if (isApiErrorResponse(err)) {
-      console.log(err.response.data);
-    } else console.log(err);
-  }
+    console.log(msg);
+    return;
+    // try {
+    //     // Using the neynarClient to publish the cast.
+    //     await neynarClient.publishCast(SIGNER_UUID, msg);
+    //     console.log("Cast published successfully");
+    // } catch (err) {
+    //     // Error handling, checking if it's an API response error.
+    //     if (isApiErrorResponse(err)) {
+    //         console.log(err.response.data);
+    //     } else console.log(err);
+    // }
 };
 
-// Initial call to publish a motivational message.
-publishCast(
-  `gm! I'm here to brighten your day with daily cheer. Look forward to a warm 'gm' everyday!`
-);
-
-// Extracting hour and minute from the PUBLISH_CAST_TIME configuration.
-const [hour, minute] = PUBLISH_CAST_TIME.split(":");
-
-// Scheduling a cron job to publish a message at a specific time every day.
+// Scheduling a cron job to report scores at the top of every hour
 cron.schedule(
-  `${minute} ${hour} * * *`, // Cron time format
-  function () {
-    publishCast(MESSAGE); // Function to execute at the scheduled time.
-  },
-  {
-    scheduled: true, // Ensure the job is scheduled.
-    timezone: TIME_ZONE, // Set the timezone for the schedule.
-  }
+    `*/1 * * * *`, // Cron time format
+    function () {
+        publishScores(); // Function to execute at the scheduled time.
+    },
+    {
+        scheduled: true, // Ensure the job is scheduled.
+    }
 );
 
 // Logging to inform that the cron job is scheduled.
 console.log(
-  `Cron job scheduled at ${PUBLISH_CAST_TIME} ${TIME_ZONE}, please don't restart your system before the scheduled time.`
+    `Cron scheduled to report new scores every hour, please don't restart your system before the scheduled time.`
 );
--- src/scores.ts
+++ src/scores.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { GameResult, GameResultSchema } from "./types";
 
-export const getLatestScores = async () => {
-    const query = `https://api.the-odds-api.com/v4/sports/icehockey_nhl/scores/?daysFrom=3&apiKey=${process.env.ODDS_API_KEY}`;
+/**
+ * Retrieves all final NHL scores from the past 24 hours.
+ */
+export const getLatestScores = async (): Promise<GameResult[]> => {
+    const query = `https://api.the-odds-api.com/v4/sports/icehockey_nhl/scores/?daysFrom=1&apiKey=${process.env.ODDS_API_KEY}`;
     const response = await axios.get(query);
 
     return response.data
